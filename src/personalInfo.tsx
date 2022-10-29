7035b3b23131f67486e20109516d2d6a832c0111
Christian Oleson <christian.oleson@imaginelearning.com>
2022-10-29 15:21:06 -0500

    Update Input Types

--- src/personalInfo.tsx
+++ src/personalInfo.tsx
@@ -1,7 +1,6 @@
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
-import React from "react";
 
 export default function PersonalInfo() : JSX.Element {
     return <Paper elevation={3} variant="outlined">
@@ -27,8 +26,9 @@ export default function PersonalInfo() : JSX.Element {
                 <TextField
                     fullWidth
                     id="outlined-basic"
-                    label="Birthdate (MM/DD/YYYY)"
+                    label="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Birthdate"
                     required
+                    type="date"
                     variant="standard" />
             </Grid>
             <Grid item xs={6} sm={4} md={4}>
@@ -70,6 +70,7 @@ export default function PersonalInfo() : JSX.Element {
                     id="outlined-basic"
                     label="Zip Code"
                     required
+                    type="number"
                     variant="standard" />
             </Grid>
             <Grid item xs={6} sm={4} md={4}>
@@ -78,6 +79,7 @@ export default function PersonalInfo() : JSX.Element {
                     id="outlined-basic"
                     label="Phone Number"
                     required
+                    type="tel"
                     variant="standard" />
             </Grid>
             <Grid item xs={6} sm={4} md={4}>
